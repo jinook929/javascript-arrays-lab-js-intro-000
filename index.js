59200c720e305871db8b84419a629adbd8a2d6a0
JINOOK JUNG <jinook929@gmail.com>
2020-11-05 23:28:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code her
-function destructivelyAppendKitten(arr, newCat) {
-  arr.push(newCat)
+function destructivelyAppendKitten(newCat) {
+  kittens.push(newCat)
+    return kittens
 }
